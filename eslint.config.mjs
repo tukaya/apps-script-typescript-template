b91d3ebe7143818f9e4d2657869369c83a8d6e80
Tuba Kaya <tubakaya@Tubas-MacBook-Pro.local>
2025-08-30 11:19:38 +0300

    initial setup with google/aside

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { defineConfig, globalIgnores } from "eslint/config";
+import googleappsscript from "eslint-plugin-googleappsscript";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "template/**/*",
+    "template-ui/**/*",
+    "**/node_modules/*",
+    "build/*",
+    "**/dist/*",
+    "**/testing",
+]), {
+    extends: compat.extends("./node_modules/gts", "prettier"),
+
+    plugins: {
+        googleappsscript,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...googleappsscript.environments.googleappsscript.globals,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        "prettier/prettier": "error",
+    },
+}]);
